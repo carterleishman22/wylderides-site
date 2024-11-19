a4a71e379b6707b229a9a37830c8c1455fe66a45
carterleishman22 <carterleishman22@gmail.com>
2024-11-19 21:22:40 +0000

    reset config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_U8Tr9e5k4', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2cl9hbj7012hj73htlpfne6gqf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://lx9pf5xelg.execute-api.eu-west-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
